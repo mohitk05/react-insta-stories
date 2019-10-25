5227e667264e1e339aa4e1da9905067aa2d60991
Mohit Karekar <karekar.mohit@gmail.com>
2019-10-25 11:53:32 +0530

    Updates in example for v2

--- packages/react-insta-stories/example/src/App.js
+++ packages/react-insta-stories/example/src/App.js
@@ -17,6 +17,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <a href="https://github.com/mohitk05/react-insta-stories/pull/32">react-insta-stories</a>
         <Stories defaultInterval={10000} stories={stories2} />
       </div>
     );
