76ea70cd8527950838c3b4dc1c40ac1733248ff4
Mohit Karekar <karekar.mohit@gmail.com>
2019-11-10 09:22:58 +0530

    Updates

---
+++ precache-manifest.a02332ea659955b816a32d0c3f9d2f17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e15e1ddb98d7b3b4fc8f0f682e48c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc82c5a8e1b98a860feb",
+    "url": "/static/css/main.6fa2d70b.chunk.css"
+  },
+  {
+    "revision": "719c6af496825fb712a0",
+    "url": "/static/js/2.b93d4fd6.chunk.js"
+  },
+  {
+    "revision": "fc82c5a8e1b98a860feb",
+    "url": "/static/js/main.8c5fd33d.chunk.js"
+  },
+  {
+    "revision": "3716bea814013ac93e90",
+    "url": "/static/js/runtime-main.7fca03b4.js"
+  }
+]);
