63df785fed3b9be7adbd18cb20ecb13aa3490e29
Mohit Karekar <karekar.mohit@gmail.com>
2019-12-13 18:04:05 +0530

    Example app updated, dev related updates

--- packages/react-insta-stories/webpack.config.js
+++ packages/react-insta-stories/webpack.config.js
@@ -49,7 +49,8 @@ module.exports = {
         library: 'Stories',
         libraryTarget: 'umd',
         publicPath: '/dist/index.js',
-        umdNamedDefine: true
+        umdNamedDefine: true,
+        globalObject: 'this'
     },
     externals: {
         // Don't bundle react or react-dom
