3f388265026d027ea9ce40b608b445de7e5ba9cd
Mohit Karekar <karekar.mohit@gmail.com>
2020-10-04 18:51:56 +0530

    Fixed default renderer loading

--- src/renderers/Default.tsx
+++ src/renderers/Default.tsx
@@ -4,46 +4,14 @@ import globalStyle from './../styles.css'
 import WithHeader from './wrappers/withHeader';
 import WithSeeMore from './wrappers/withSeeMore';
 
-export const renderer: Renderer = ({ story, action, isPaused, config }) => {
-    const [loaded, setLoaded] = React.useState(false);
-    const { width, height, loader, storyStyles } = config;
-
-    let computedStyles = {
-        ...styles.storyContent,
-        ...(storyStyles || {})
-    }
-
-    const imageLoaded = () => {
-        setLoaded(true);
+export const renderer: Renderer = ({ story, action }) => {
+    React.useEffect(() => {
         action('play');
-    }
+    }, [story])
 
-    return <WithHeader story={story} globalHeader={config.header}>
-        <WithSeeMore story={story} action={action}>
-            <div>
-                <img style={computedStyles} src={story.url} onLoad={imageLoaded} />
-                {!loaded && (
-                    <div
-                        style={{
-                            width: width,
-                            height: height,
-                            position: "absolute",
-                            left: 0,
-                            top: 0,
-                            background: "rgba(0, 0, 0, 0.9)",
-                            zIndex: 9,
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                            color: "#ccc"
-                        }}
-                    >
-                        {loader || <div className={globalStyle.spinner} />}
-                    </div>
-                )}
-            </div>
-        </WithSeeMore>
-    </WithHeader>
+    return <div style={styles.storyContent}>
+        <p style={styles.text}>This story could not be loaded.</p>
+    </div>
 }
 
 const styles = {
@@ -53,10 +21,15 @@ const styles = {
         overflow: "hidden"
     },
     storyContent: {
-        width: "auto",
-        maxWidth: "100%",
+        width: "100%",
         maxHeight: "100%",
         margin: "auto"
+    },
+    text: {
+        textAlign: 'center',
+        color: 'white',
+        width: '90%',
+        margin: 'auto'
     }
 };
 
