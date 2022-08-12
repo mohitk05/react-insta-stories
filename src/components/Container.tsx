db61d39aae5beee3153300653e6445c3083ca639
Freddie Horne <freddie@letsdothis.com>
2022-08-12 11:06:35 +0100

    testing to get rid of pause at beginning

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,158 +1,200 @@
-import React, { useContext, useState, useRef, useEffect } from 'react'
-import GlobalContext from './../context/Global'
-import StoriesContext from './../context/Stories'
-import ProgressContext from './../context/Progress'
-import Story from './Story'
-import ProgressArray from './ProgressArray'
-import { GlobalCtx, StoriesContext as StoriesContextInterface } from './../interfaces'
+import React, { useContext, useState, useRef, useEffect } from "react";
+import GlobalContext from "./../context/Global";
+import StoriesContext from "./../context/Stories";
+import ProgressContext from "./../context/Progress";
+import Story from "./Story";
+import ProgressArray from "./ProgressArray";
+import {
+  GlobalCtx,
+  StoriesContext as StoriesContextInterface,
+} from "./../interfaces";
 
 export default function () {
-    const [currentId, setCurrentId] = useState<number>(0)
-    const [pause, setPause] = useState<boolean>(true)
-    const [bufferAction, setBufferAction] = useState<boolean>(true)
-    const [videoDuration, setVideoDuration] = useState<number>(0)
+  const [currentId, setCurrentId] = useState<number>(0);
+  const [pause, setPause] = useState<boolean>(false);
+  const [bufferAction, setBufferAction] = useState<boolean>(false);
+  const [videoDuration, setVideoDuration] = useState<number>(0);
 
-    let mousedownId = useRef<any>();
-    let isMounted = useRef<boolean>(true);
+  let mousedownId = useRef<any>();
+  let isMounted = useRef<boolean>(true);
 
-    const { width, height, loop, currentIndex, isPaused, keyboardNavigation, preventDefault, storyContainerStyles = {} } = useContext<GlobalCtx>(GlobalContext);
-    const { stories } = useContext<StoriesContextInterface>(StoriesContext);
+  const {
+    width,
+    height,
+    loop,
+    currentIndex,
+    isPaused,
+    keyboardNavigation,
+    preventDefault,
+    storyContainerStyles = {},
+  } = useContext<GlobalCtx>(GlobalContext);
+  const { stories } = useContext<StoriesContextInterface>(StoriesContext);
 
-    useEffect(() => {
-        if (typeof currentIndex === 'number') {
-            if (currentIndex >= 0 && currentIndex < stories.length) {
-                setCurrentIdWrapper(() => currentIndex)
-            } else {
-                console.error('Index out of bounds. Current index was set to value more than the length of stories array.', currentIndex)
-            }
-        }
-    }, [currentIndex])
-
-    useEffect(() => {
-        if (typeof isPaused === 'boolean') {
-            setPause(isPaused)
-        }
-    }, [isPaused])
-
-    useEffect(() => {
-        const isClient = (typeof window !== 'undefined' && window.document);
-        if (isClient && (typeof keyboardNavigation === 'boolean' && keyboardNavigation)) {
-            document.addEventListener("keydown", handleKeyDown);
-            return () => {
-                document.removeEventListener("keydown", handleKeyDown);
-            }
-        }
-    }, [keyboardNavigation]);
-
-    // Cleanup mounted state - for issue #130 (https://github.com/mohitk05/react-insta-stories/issues/130)
-    useEffect(() => {
-        return () => {
-            isMounted.current = false;
-        }
-    }, []);
-
-    const handleKeyDown = (e: KeyboardEvent) => {
-        if (e.key === 'ArrowLeft') {
-            previous()
-        }
-        else if (e.key === 'ArrowRight') {
-            next()
-        }
+  useEffect(() => {
+    if (typeof currentIndex === "number") {
+      if (currentIndex >= 0 && currentIndex < stories.length) {
+        setCurrentIdWrapper(() => currentIndex);
+      } else {
+        console.error(
+          "Index out of bounds. Current index was set to value more than the length of stories array.",
+          currentIndex
+        );
+      }
     }
+  }, [currentIndex]);
 
-    const toggleState = (action: string, bufferAction?: boolean) => {
-        setPause(action === 'pause')
-        setBufferAction(!!bufferAction)
+  useEffect(() => {
+    if (typeof isPaused === "boolean") {
+      setPause(isPaused);
     }
+  }, [isPaused]);
 
-    const setCurrentIdWrapper = (callback) => {
-        setCurrentId(callback);
-        toggleState('pause', true);
+  useEffect(() => {
+    const isClient = typeof window !== "undefined" && window.document;
+    if (
+      isClient &&
+      typeof keyboardNavigation === "boolean" &&
+      keyboardNavigation
+    ) {
+      document.addEventListener("keydown", handleKeyDown);
+      return () => {
+        document.removeEventListener("keydown", handleKeyDown);
+      };
     }
+  }, [keyboardNavigation]);
 
-    const previous = () => {
-        setCurrentIdWrapper(prev => prev > 0 ? prev - 1 : prev)
-    }
-
-    const next = () => {
-        if (isMounted.current) {
-            if (loop) {
-                updateNextStoryIdForLoop()
-            } else {
-                updateNextStoryId()
-            }
-        }
+  // Cleanup mounted state - for issue #130 (https://github.com/mohitk05/react-insta-stories/issues/130)
+  useEffect(() => {
+    return () => {
+      isMounted.current = false;
     };
+  }, []);
 
-    const updateNextStoryIdForLoop = () => {
-        setCurrentIdWrapper(prev => (prev + 1) % stories.length)
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "ArrowLeft") {
+      previous();
+    } else if (e.key === "ArrowRight") {
+      next();
     }
+  };
 
-    const updateNextStoryId = () => {
-        setCurrentIdWrapper(prev => {
-            if (prev < stories.length - 1) return prev + 1
-            return prev
-        })
+  const toggleState = (action: string, bufferAction?: boolean) => {
+    setPause(action === "pause");
+    setBufferAction(!!bufferAction);
+  };
+
+  const setCurrentIdWrapper = (callback) => {
+    setCurrentId(callback);
+    toggleState("pause", true);
+  };
+
+  const previous = () => {
+    setCurrentIdWrapper((prev) => (prev > 0 ? prev - 1 : prev));
+  };
+
+  const next = () => {
+    if (isMounted.current) {
+      if (loop) {
+        updateNextStoryIdForLoop();
+      } else {
+        updateNextStoryId();
+      }
     }
+  };
 
-    const debouncePause = (e: React.MouseEvent | React.TouchEvent) => {
-        e.preventDefault()
-        mousedownId.current = setTimeout(() => {
-            toggleState('pause')
-        }, 200)
+  const updateNextStoryIdForLoop = () => {
+    setCurrentIdWrapper((prev) => (prev + 1) % stories.length);
+  };
+
+  const updateNextStoryId = () => {
+    setCurrentIdWrapper((prev) => {
+      if (prev < stories.length - 1) return prev + 1;
+      return prev;
+    });
+  };
+
+  const debouncePause = (e: React.MouseEvent | React.TouchEvent) => {
+    e.preventDefault();
+    mousedownId.current = setTimeout(() => {
+      toggleState("pause");
+    }, 200);
+  };
+
+  const mouseUp = (type: string) => (
+    e: React.MouseEvent | React.TouchEvent
+  ) => {
+    e.preventDefault();
+    mousedownId.current && clearTimeout(mousedownId.current);
+    if (pause) {
+      toggleState("play");
+    } else {
+      type === "next" ? next() : previous();
     }
+  };
 
-    const mouseUp = (type: string) => (e: React.MouseEvent | React.TouchEvent) => {
-        e.preventDefault()
-        mousedownId.current && clearTimeout(mousedownId.current)
-        if (pause) {
-            toggleState('play')
-        } else {
-            type === 'next' ? next() : previous()
-        }
-    }
+  const getVideoDuration = (duration: number) => {
+    setVideoDuration(duration * 1000);
+  };
 
-    const getVideoDuration = (duration: number) => {
-        setVideoDuration(duration * 1000)
-    }
-
-    return (
-        <div style={{ ...styles.container, ...storyContainerStyles, ...{ width, height } }}>
-            <ProgressContext.Provider value={{
-                bufferAction: bufferAction,
-                videoDuration: videoDuration,
-                currentId,
-                pause,
-                next
-            }}>
-                <ProgressArray />
-            </ProgressContext.Provider>
-            <Story
-                action={toggleState}
-                bufferAction={bufferAction}
-                playState={pause}
-                story={stories[currentId]}
-                getVideoDuration={getVideoDuration}
-            />
-            {!preventDefault && <div style={styles.overlay}>
-              <div style={{ width: '50%', zIndex: 999 }} onTouchStart={debouncePause} onTouchEnd={mouseUp('previous')} onMouseDown={debouncePause} onMouseUp={mouseUp('previous')} />
-              <div style={{ width: '50%', zIndex: 999 }} onTouchStart={debouncePause} onTouchEnd={mouseUp('next')} onMouseDown={debouncePause} onMouseUp={mouseUp('next')} />
-            </div>}
+  return (
+    <div
+      style={{
+        ...styles.container,
+        ...storyContainerStyles,
+        ...{ width, height },
+      }}
+    >
+      <ProgressContext.Provider
+        value={{
+          bufferAction: bufferAction,
+          videoDuration: videoDuration,
+          currentId,
+          pause,
+          next,
+        }}
+      >
+        <ProgressArray />
+      </ProgressContext.Provider>
+      <Story
+        action={toggleState}
+        bufferAction={bufferAction}
+        playState={pause}
+        story={stories[currentId]}
+        getVideoDuration={getVideoDuration}
+      />
+      {!preventDefault && (
+        <div style={styles.overlay}>
+          <div
+            style={{ width: "50%", zIndex: 999 }}
+            onTouchStart={debouncePause}
+            onTouchEnd={mouseUp("previous")}
+            onMouseDown={debouncePause}
+            onMouseUp={mouseUp("previous")}
+          />
+          <div
+            style={{ width: "50%", zIndex: 999 }}
+            onTouchStart={debouncePause}
+            onTouchEnd={mouseUp("next")}
+            onMouseDown={debouncePause}
+            onMouseUp={mouseUp("next")}
+          />
         </div>
-    )
+      )}
+    </div>
+  );
 }
 
 const styles = {
-    container: {
-        display: 'flex',
-        flexDirection: 'column',
-        background: '#111',
-        position: 'relative'
-    },
-    overlay: {
-        position: 'absolute',
-        height: 'inherit',
-        width: 'inherit',
-        display: 'flex'
-    }
-}
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    background: "#111",
+    position: "relative",
+  },
+  overlay: {
+    position: "absolute",
+    height: "inherit",
+    width: "inherit",
+    display: "flex",
+  },
+};
