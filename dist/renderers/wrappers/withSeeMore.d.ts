4cb36a6f67324a5d535753fc31f2336e7021ad77
dor-squadded <76150116+dor-squadded@users.noreply.github.com>
2022-10-11 16:41:08 +0300

    Delete dist directory

--- dist/renderers/wrappers/withSeeMore.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Action, SeeMoreProps, Story } from '../../interfaces';
-declare const withSeeMore: React.FC<{
-    story: Story;
-    action: Action;
-    customCollapsed?: SeeMoreProps["customCollapsed"];
-}>;
-export default withSeeMore;
