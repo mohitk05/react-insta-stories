f236f90c26081e93b9aa2bd1a5adcd7f73858f75
dor-squadded <76150116+dor-squadded@users.noreply.github.com>
2022-10-11 16:42:44 +0300

    Add files via upload

---
+++ dist/components/ProgressWrapper.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react" />
+import { ProgressWrapperProps } from './../interfaces';
+declare const ProgressWrapper: (props: ProgressWrapperProps) => JSX.Element;
+export default ProgressWrapper;
