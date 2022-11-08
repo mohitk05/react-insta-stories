96de0e250dba2f7ffd81db58ba09d90a00e0ab6c
dor-squadded <76150116+dor-squadded@users.noreply.github.com>
2022-11-08 15:17:21 +0200

    Add files via upload

---
+++ dist/src/components/Header.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react" />
+import { HeaderProps } from './../interfaces';
+declare const Header: ({ profileImage, heading, subheading }: HeaderProps) => JSX.Element;
+export default Header;
---
+++ dist/src/components/Progress.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react" />
+import { ProgressProps } from './../interfaces';
+declare const _default: (props: ProgressProps) => JSX.Element;
+export default _default;
