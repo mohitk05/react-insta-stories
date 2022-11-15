d638a4b0d3805e2fe9f1a8edc010005bb344b463
Dor Kuperman <dorkup@gmail.com>
2022-11-15 16:20:38 +0200

    udpate

--- src/src/components/Spinner.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import SpinnerSVG from './../assets/puff.svg';
-
-const Spinner = () => {
-    return <SpinnerSVG />
-}
-
-export default Spinner;
--- src/src/renderers/index.ts
+++
@@ -1,6 +0,0 @@
-import image from './Image';
-import video from './Video';
-import defaultRenderer from './Default';
-import autoplayContent from './AutoPlayContent';
-
-export const renderers = [image, video, autoplayContent, defaultRenderer];
