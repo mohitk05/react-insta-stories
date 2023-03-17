71eb334ed3d091ba0134020ae2087e1583c40092
mohitk05 <karekar.mohit@gmail.com>
2023-03-17 16:20:28 +0100

    Upgrade dependencies, fix React 18 bug for progress bar

--- src/renderers/Default.tsx
+++ src/renderers/Default.tsx
@@ -1,38 +1,40 @@
-import * as React from 'react';
-import { Renderer, Tester } from './../interfaces';
+import * as React from "react";
+import { Renderer, Tester } from "./../interfaces";
 
 export const renderer: Renderer = ({ story, action }) => {
-    React.useEffect(() => {
-        action('play');
-    }, [story])
+  React.useEffect(() => {
+    action("play");
+  }, [story]);
 
-    return <div style={styles.storyContent}>
-        <p style={styles.text}>This story could not be loaded.</p>
+  return (
+    <div style={styles.storyContent}>
+      <p style={styles.text}>This story could not be loaded.</p>
     </div>
-}
+  );
+};
 
 const styles = {
-    storyContent: {
-        width: "100%",
-        maxHeight: "100%",
-        margin: "auto"
-    },
-    text: {
-        textAlign: 'center',
-        color: 'white',
-        width: '90%',
-        margin: 'auto'
-    }
+  storyContent: {
+    width: "100%",
+    maxHeight: "100%",
+    margin: "auto",
+  },
+  text: {
+    textAlign: "center" as "center",
+    color: "white",
+    width: "90%",
+    margin: "auto",
+  },
 };
 
 export const tester: Tester = () => {
-    return {
-        condition: true,
-        priority: 1
-    };
-}
+  return {
+    condition: true,
+    priority: 1,
+  };
+};
 
 export default {
-    renderer,
-    tester
-}
+  renderer,
+  tester,
+};
